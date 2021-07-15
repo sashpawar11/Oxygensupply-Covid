004a9070fe17647409456c7bb3fcdc6508d34918
siddhant desai <sidesai2001@gmail.com>
2021-07-16 00:36:19 +0530

    oxygen view database

--- CovidApp/OxygenSupplierView.Designer.cs
+++ CovidApp/OxygenSupplierView.Designer.cs
@@ -29,63 +29,70 @@ namespace CovidApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.hosp_name = new System.Windows.Forms.TextBox();
+            this.oxy_price = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.oxy_stock = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // hosp_name
+            // oxy_price
             // 
-            this.hosp_name.Location = new System.Drawing.Point(114, 56);
-            this.hosp_name.Name = "hosp_name";
-            this.hosp_name.Size = new System.Drawing.Size(239, 22);
-            this.hosp_name.TabIndex = 8;
+            this.oxy_price.Location = new System.Drawing.Point(86, 46);
+            this.oxy_price.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.oxy_price.Name = "oxy_price";
+            this.oxy_price.Size = new System.Drawing.Size(180, 20);
+            this.oxy_price.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 59);
+            this.label2.Location = new System.Drawing.Point(45, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "Price :";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // oxy_stock
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 179);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 22);
-            this.textBox1.TabIndex = 9;
+            this.oxy_stock.Location = new System.Drawing.Point(88, 145);
+            this.oxy_stock.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.oxy_stock.Name = "oxy_stock";
+            this.oxy_stock.Size = new System.Drawing.Size(180, 20);
+            this.oxy_stock.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 179);
+            this.label1.Location = new System.Drawing.Point(45, 145);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 10;
             this.label1.Text = "Stock :";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(63, 438);
+            this.button1.Location = new System.Drawing.Point(47, 356);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(293, 37);
+            this.button1.Size = new System.Drawing.Size(220, 30);
             this.button1.TabIndex = 11;
             this.button1.Text = "Update";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 287);
+            this.label3.Location = new System.Drawing.Point(45, 233);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 12;
             this.label3.Text = "Status :";
             // 
@@ -96,23 +103,25 @@ namespace CovidApp
             "Available",
             "Unavailable",
             "Out of Stock"});
-            this.comboBox1.Location = new System.Drawing.Point(122, 287);
+            this.comboBox1.Location = new System.Drawing.Point(92, 233);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(234, 24);
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
             this.comboBox1.TabIndex = 13;
             // 
             // OxygenSupplierView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 651);
+            this.ClientSize = new System.Drawing.Size(884, 529);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.hosp_name);
+            this.Controls.Add(this.oxy_stock);
+            this.Controls.Add(this.oxy_price);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OxygenSupplierView";
             this.Text = "OxygenSupplierView";
             this.Load += new System.EventHandler(this.OxygenSupplierView_Load);
@@ -123,9 +132,9 @@ namespace CovidApp
 
         #endregion
 
-        private System.Windows.Forms.TextBox hosp_name;
+        private System.Windows.Forms.TextBox oxy_price;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox oxy_stock;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
--- CovidApp/OxygenSupplierView.cs
+++ CovidApp/OxygenSupplierView.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 using System.Windows.Forms;
 
 namespace CovidApp
@@ -25,6 +26,26 @@ namespace CovidApp
         private void OxygenSupplierView_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String price = oxy_price.Text;
+            String stock = oxy_stock.Text;
+            String status = comboBox1.SelectedItem.ToString();
+
+            string connectionString = @"provider = Microsoft.ACE.OLEDB.12.0; 
+                            Data source = D:\Oxygen supplier.xlsx; 
+                            Extended Properties = 'Excel 8.0'";
+            OleDbConnection oleDbConnection = new OleDbConnection(connectionString);
+
+            oleDbConnection.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            string query = "INSERT INTO [Sheet1$] ([Price], [Stock], [Status]) VALUES('" + price + "','" + stock + "','" + status + "')";
+            cmd.Connection = oleDbConnection;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            oleDbConnection.Close();
         }
     }
 }
